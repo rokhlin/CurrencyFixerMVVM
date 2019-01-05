e5e5607690a49d08003d49bf598fed2936cd95e0
Anton <rohlinav@gmail.com>
2019-01-05 17:04:23 +0200

    Full Currency entity

---
+++ app/src/main/java/com/selfapps/currencyfixermvvm/entity/FullCurrency.java
@@ -0,0 +1,36 @@
+package com.selfapps.currencyfixermvvm.entity;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Map;
+
+public class FullCurrency  {
+    
+	@SerializedName("timestamp")
+	@Expose
+	public long timestamp;
+    
+	@SerializedName("date")
+	@Expose
+	public String date;
+    
+	@SerializedName("base")
+	@Expose
+	public String base;
+    
+	@SerializedName("rates")
+	@Expose
+	public Map<String,Double> rates;
+    
+    public FullCurrency() {}
+
+	@Override
+	public String toString() {
+		return "FullCurrency{" +
+				"timestamp=" + timestamp +
+				", date='" + date + '\'' +
+				", base='" + base + '\'' +
+				", rates=" + rates +
+				'}';
+	}
+}
