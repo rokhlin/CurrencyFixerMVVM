2fdeef9aba67ae1d952a8fc8a92597b19d49afd8
Anton <rohlinav@gmail.com>
2019-01-07 16:09:36 +0200

    +Converter

--- app/src/main/java/com/selfapps/currencyfixermvvm/ConverterFragment.java
+++ app/src/main/java/com/selfapps/currencyfixermvvm/ConverterFragment.java
@@ -3,22 +3,41 @@ package com.selfapps.currencyfixermvvm;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.selfapps.currencyfixermvvm.data.entity.FullCurrency;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class ConverterFragment extends Fragment {
 
     private ConverterViewModel mViewModel;
     private LiveData<FullCurrency> fullCurrency;
-    private TextView text;
+    private LiveData<Map<String,String>> definitions;
+
+
+    private Spinner spFrom,spTo;
+    ArrayAdapter<String> adapterFrom, adapterTo;
+    private TextView convertion;
+    private EditText exchangeValue;
+
     public static ConverterFragment newInstance() {
         return new ConverterFragment();
     }
@@ -27,9 +46,69 @@ public class ConverterFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.converter_fragment, container, false);
+        convertion = view.findViewById(R.id.tv_convertion);
 
-        text = view.findViewById(R.id.fragment_text);
+        initEditText(view);
+        spFrom = view.findViewById(R.id.spinner_from);
+        spTo = view.findViewById(R.id.spinner_to);
+
         return view;
+    }
+
+    private void initEditText(View view) {
+        exchangeValue = view.findViewById(R.id.et_value);
+        exchangeValue.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                calculateUpdates();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private void initSpinners(Context c) {
+        if(definitions != null){
+            List<String> keys = new ArrayList<>(definitions.getValue().keySet());
+            adapterFrom = new ArrayAdapter<>(c,
+                    android.R.layout.simple_spinner_item, keys);
+            adapterFrom.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spFrom.setAdapter(adapterFrom);
+            spFrom.setSelection(5);
+
+            adapterTo = new ArrayAdapter<>(c,
+                    android.R.layout.simple_spinner_item, keys);
+            adapterTo.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spTo.setAdapter(adapterTo);
+            spTo.setSelection(7);
+
+            AdapterView.OnItemSelectedListener listener = new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) { calculateUpdates(); }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) { }};
+
+            spTo.setOnItemSelectedListener(listener);
+            spFrom.setOnItemSelectedListener(listener);
+        }
+
+    }
+
+    private void calculateUpdates() {
+        double amount = Double.parseDouble(String.valueOf(exchangeValue.getText()));
+        double exchangeRateFrom = fullCurrency.getValue().rates.get(spFrom.getSelectedItem().toString());
+        double exchangeRateTo = fullCurrency.getValue().rates.get(spTo.getSelectedItem().toString());
+
+        convertion.setText(Util.convertValue(amount,exchangeRateFrom,exchangeRateTo)+"");
     }
 
     @Override
@@ -41,7 +120,20 @@ public class ConverterFragment extends Fragment {
         fullCurrency.observe(this, new Observer<FullCurrency>() {
             @Override
             public void onChanged(@Nullable FullCurrency fullCurrency) {
-                if(fullCurrency!= null) text.setText(fullCurrency.toString());
+               // if(fullCurrency!= null) text.setText(fullCurrency.toString());
+            }
+        });
+
+
+        definitions = mViewModel.getDefinitions();
+        definitions.observe(this, new Observer<Map<String, String>>() {
+            @Override
+            public void onChanged(@Nullable Map<String, String> def) {
+                initSpinners(getContext());
+//               // adapterFrom.clear();
+//                adapterFrom.addAll((String[]) Objects.requireNonNull(def.keySet().toArray()));
+//                //adapterTo.clear();
+//                adapterTo.addAll((String[]) Objects.requireNonNull(def.keySet().toArray()));
             }
         });
 
--- app/src/main/java/com/selfapps/currencyfixermvvm/ConverterViewModel.java
+++ app/src/main/java/com/selfapps/currencyfixermvvm/ConverterViewModel.java
@@ -1,30 +1,61 @@
 package com.selfapps.currencyfixermvvm;
 
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
-import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.preference.PowerPreference;
 import com.selfapps.currencyfixermvvm.data.FixerRepository;
+import com.selfapps.currencyfixermvvm.data.entity.DefinitionsResponse;
 import com.selfapps.currencyfixermvvm.data.entity.FullCurrency;
+import com.selfapps.currencyfixermvvm.data.repository.CurrencyDao;
+import com.selfapps.currencyfixermvvm.data.repository.Database;
+
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class ConverterViewModel extends ViewModel {
+public class ConverterViewModel extends AndroidViewModel {
 
+    private static final String CURRENCY_LIST = "currency_list";
     private MutableLiveData<FullCurrency> fullCurrencyLiveData = new MutableLiveData<>();
+    private MutableLiveData<Map<String,String>> definitions = new MutableLiveData<>();
+    private CurrencyDao db ;
+
+    public ConverterViewModel(@NonNull Application application) {
+        super(application);
+        db = Database.getDao(application.getApplicationContext());
+    }
+
+
+
 
 
     public LiveData<FullCurrency> getLatest(){
-         FixerRepository.getAPI().getLatest().enqueue(new Callback<FullCurrency>() {
+        FullCurrency latestCurrency = db.findCurrency(Util.getFormattedDate()).getValue();
+        if(latestCurrency != null)
+            fullCurrencyLiveData.postValue(latestCurrency);
+        else
+            FixerRepository.getAPI().getLatest().enqueue(new Callback<FullCurrency>() {
              @Override
              public void onResponse(Call<FullCurrency> call, Response<FullCurrency> response) {
                  if(response != null){
-                     Log.d("TAG", "GET_LATEST response: "+response.raw());
+                     final FullCurrency latestCurr = response.body();
                      Log.d("TAG", "GET_LATEST body: "+response.body());
                      fullCurrencyLiveData.postValue(response.body());
+
+                     new Thread(new Runnable() {
+                         @Override
+                         public void run() {
+                             db.insertCurrency(latestCurr);
+                         }
+                     }).start();
+
                  }
              }
 
@@ -36,23 +67,27 @@ public class ConverterViewModel extends ViewModel {
          return fullCurrencyLiveData;
     }
 
-    public LiveData<FullCurrency> getDefinitions(){
-        FixerRepository.getAPI().getLatest().enqueue(new Callback<FullCurrency>() {
-            @Override
-            public void onResponse(Call<FullCurrency> call, Response<FullCurrency> response) {
-                if(response != null){
-                    Log.d("TAG", "GET_LATEST response: "+response.raw());
-                    Log.d("TAG", "GET_LATEST body: "+response.body());
-                    fullCurrencyLiveData.postValue(response.body());
+    public LiveData<Map<String,String>> getDefinitions(){
+        String currList = PowerPreference.defult().getString(CURRENCY_LIST, null);
+        if(currList != null)
+            definitions.postValue(Util.toMap(currList));
+        else
+            FixerRepository.getAPI().getDefinitions().enqueue(new Callback<DefinitionsResponse>() {
+                @Override
+                public void onResponse(Call<DefinitionsResponse> call, Response<DefinitionsResponse> response) {
+                    if(response.body() != null)
+                        definitions.postValue(response.body().symbols);
+                        PowerPreference.defult().put(CURRENCY_LIST,Util.fromMap(response.body().symbols));
+                    Log.d("TAG", "GET_DEFINITIONS body: "+response.body());
                 }
-            }
 
-            @Override
-            public void onFailure(Call<FullCurrency> call, Throwable t) {
-                Log.d("TAG", "GET_LATEST error "+t.getMessage());
-            }
-        });
-        return fullCurrencyLiveData;
+                @Override
+                public void onFailure(Call<DefinitionsResponse> call, Throwable t) {
+                    Log.d("TAG", "GET_DEFINITIONS error "+t.getMessage());
+                }
+            });
+
+            return definitions;
     }
 
 }
